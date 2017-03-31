8e51c6b52f94dc4ffd7d94c0506d7bb441dc00cd
jmy3028 <jmy3028@naver.com>
2017-03-31 16:50:18 +0900

    ListFragmentAdapter 완성 ListFragment에 대입시켜서 뿌려줘야

--- app/src/main/java/com/practice/jmy3028/gmappracticeapplication/adapters/ListFragmentAdapter.java
+++ app/src/main/java/com/practice/jmy3028/gmappracticeapplication/adapters/ListFragmentAdapter.java
@@ -26,6 +26,15 @@ public class ListFragmentAdapter extends BaseExpandableListAdapter {
     private List<ListModel> mChildList;
     private HashMap<DtModel,List<ListModel>> mChildHashMap;
 
+    public ListFragmentAdapter(Context context, List<DtModel> mParentList,
+                               List<ListModel> mChildList, HashMap<DtModel,
+                                List<ListModel>> mChildHashMap) {
+        this.context = context;
+        this.mParentList = mParentList;
+        this.mChildList = mChildList;
+        this.mChildHashMap = mChildHashMap;
+    }
+
     @Override
     public int getGroupCount() {
         return mParentList.size();
@@ -99,24 +108,45 @@ public class ListFragmentAdapter extends BaseExpandableListAdapter {
             convertView = LayoutInflater.from(context)
                     .inflate(R.layout.item_child,parent,false);
 
-            TextView dtTextView = (TextView) convertView.findViewById(R.id.dt_text);
+            ImageView mWeatherImage = (ImageView) convertView.findViewById(R.id.weather_image);
+            TextView mWeatherText = (TextView) convertView.findViewById(R.id.weather_text);
+            TextView mTempText = (TextView) convertView.findViewById(R.id.temp_text);
+            TextView mWindSpeedText = (TextView) convertView.findViewById(R.id.windSpeed_text);
+            TextView mWindDirText = (TextView) convertView.findViewById(R.id.windDir_text);
+            TextView mPressureText = (TextView) convertView.findViewById(R.id.pressure_text);
+            TextView mHumidityText = (TextView) convertView.findViewById(R.id.humidity_text);
 
-            groupViewHolder.dtTextView = dtTextView;
 
-            convertView.setTag(groupViewHolder);
+            childViewHolder.mWeatherImage = mWeatherImage;
+            childViewHolder.mWeatherText = mWeatherText;
+            childViewHolder.mTempText = mTempText;
+            childViewHolder.mWindSpeedText = mWindSpeedText;
+            childViewHolder.mWindDirText = mWindDirText;
+            childViewHolder.mPressureText = mPressureText;
+            childViewHolder.mHumidityText = mHumidityText;
+
+
+            convertView.setTag(childViewHolder);
 
         }else {
-            groupViewHolder = (GroupViewHolder) convertView.getTag();
+            childViewHolder = (ChildViewHolder) convertView.getTag();
         }
 
-        DtModel dtModel = mParentList.get(groupPosition);
+        ListModel listModel = mChildList.get(groupPosition);
 
-        groupViewHolder.dtTextView.setText(dtModel.getmDtdata());
+        //이미지 받아오기
+//        childViewHolder.mWeatherImage.;
 
+        //데이터값을 넣어서 화면에 뿌려주는 공간
+        childViewHolder.mWeatherText.setText(listModel.getmWeather());
+        childViewHolder.mTempText.setText(String.valueOf(listModel.getmTemp()));
+        childViewHolder.mWindSpeedText.setText(listModel.getmWindSpeed());
+        childViewHolder.mWindDirText.setText(String.valueOf(listModel.getmWindDir()));
+        childViewHolder.mPressureText.setText(listModel.getmPressure());
+        childViewHolder.mHumidityText.setText(listModel.getmHumidity());
 
         return convertView;
 
-        return convertView;
     }
 
     @Override
--- app/src/main/java/com/practice/jmy3028/gmappracticeapplication/fragments/ListFragment.java
+++ app/src/main/java/com/practice/jmy3028/gmappracticeapplication/fragments/ListFragment.java
@@ -6,17 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
+import com.practice.jmy3028.gmappracticeapplication.DtModel;
+import com.practice.jmy3028.gmappracticeapplication.ListModel;
 import com.practice.jmy3028.gmappracticeapplication.R;
+import com.practice.jmy3028.gmappracticeapplication.adapters.ListFragmentAdapter;
 import com.practice.jmy3028.gmappracticeapplication.model.WeatherMain;
 import com.practice.jmy3028.gmappracticeapplication.model2.Example;
 import com.practice.jmy3028.gmappracticeapplication.model2.List;
+
+import java.util.HashMap;
 
 /**
  * Created by jmy3028 on 17. 3. 22.
  */
 
 public class ListFragment extends Fragment {
+
+    private java.util.List<DtModel> mDtData;
+    private java.util.List<ListModel> mListData;
+    private HashMap<DtModel, java.util.List<ListModel>> mGroupData;
+
 
     //날씨에 대한 모든 데이터들을 이쪽에서 받기
     public static ListFragment newInstance(Example data) {
@@ -36,5 +47,14 @@ public class ListFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Example example = (Example) getArguments().getSerializable("data2");
+
+
+
+        ExpandableListView listView = (ExpandableListView) view.findViewById(R.id.expanded_list);
+        ListFragmentAdapter adapter = new ListFragmentAdapter();
+
+        listView.setAdapter(adapter);
     }
 }
